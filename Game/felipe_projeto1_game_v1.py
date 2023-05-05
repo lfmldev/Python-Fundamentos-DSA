87a5d7e7e6efbf25f1b047f353a26670dc997703
Luiz Felipe Martins de Lima <lfml.dev@gmail.com>
2023-05-05 20:38:04 -0300

    Update felipe_projeto1_game_v1.py

--- Game/felipe_projeto1_game_v1.py
+++ Game/felipe_projeto1_game_v1.py
@@ -2,8 +2,8 @@
 
 # Importando pacotes
 
-import random # utilizado para escolher a palavra de maneira aleatória
-from os import system, name # serve para identificar o sistema operacional
+import random 								# utilizado para escolher a palavra de maneira aleatória
+from os import system, name 				# serve para identificar o sistema operacional
 
 # Função para limpar a tela de execução
 
@@ -11,8 +11,8 @@ def limpa_tela():
 
 	# Windows
 	if name == 'nt': 
-		_= system('cls') # o underline serve para liberar o return da função system, 
-						 # não precisamos do return só do resultado
+		_= system('cls') 					# o underline serve para liberar o return da função system, 
+						 					# não precisamos do return só do resultado
 	# Mac ou Linux
 	else:
 		_= system('clear')
@@ -36,24 +36,45 @@ def game():
 	letras_descobertas = ['_' for letras in palavra]
 
 	# Número de tentativas
-	tentativas = 6
+	chances = 6
 
 	# Lista para as letras erradas
 	letras_erradas = []
 
-	# Loop enquanto para definir o limite de tentativas
-	while tentativas > 0:
+	# Loop enquanto o número de chances for maior que zero
+	while chances > 0:
 		# Prints de tela para o usuário
-		print(" ")
-		pass
+		print(" ".join(letras_descobertas)) 				# junção, vai juntar o espaço as letras descobertas
+		print("\nTentativas restantes: ", chances) 			# n° de chances
+		print("Letras erradas:", " ".join(letras_erradas)) 	# junção das letras erradas
+
+		# Tentativas
+		tentativa = input("\nDigite uma letra: ").lower() 	# Recebo o input do usuário e defino como minúsculo
+
+		# Condicional para verificar a existência da letra na palavra
+
+		if tentativa in palavra: 							# se a letra estiver na palavra
+			index = 0  										# cria um index zerado
+			for letra in palavra: 							# para cada letra na palavra
+				if tentativa == letra:  					# se a letra ecolhida for igual a letra da palavra
+					letras_descobertas[index] = letra 		# coloco a letra descoberta no índice da letra e retiro o "_"
+				index += 1									# incremento o índice a cada loop
+
+		else: 												# Se não estiver na palavra
+			chances -= 1  									# Decremento das chances
+			letras_erradas.append(tentativa)				# e faço um append na lista criada para as letras erradas
+
+		# Condicional para terminar o game
+		if "_" not in letras_descobertas:					# se não tiver mais underscore nas letras descobertas
+			print("\nVocê venceu, a palavra é: ", palavra)  # Print
+			break											# Break, para o programa
+	
+	# Condicional caso o número de chances excedida
+	if "_" in letras_descobertas:
+		print("\nVocê perdeu, a palavra era:", palavra)
 
 
-
-
-
-
-
-
-
-
-
+# Bloco main, serve para dizer ao interpretador que isso é um programa, um módulo python
+if __name__ == "__main__":
+	game()
+	print("\nParabéns. Você está aprendendo ao programar!! :)\n")
